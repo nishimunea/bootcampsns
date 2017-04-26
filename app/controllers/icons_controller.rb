f5ce29ffecead850a05e746e686b3a34b2dc0dc3
nishimunea <nishimunea@gmail.com>
2017-04-27 07:29:33 +0900

    bug fixes

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -4,13 +4,14 @@ class IconsController < ApplicationController
     file_name = params[:image].original_filename.downcase
     mime_type = params[:image].content_type.downcase
     if !!file_name.match(/png|jpeg|jpg|gif/) and mime_type.start_with? 'image/'
-      image_path = "#{Rails.root}/public/icons/#{file_name}"
+      dest_file_name = "#{SecureRandom.uuid}#{File.extname(file_name)}"
+      image_path = "#{Rails.root}/public/icons/#{dest_file_name}"
       FileUtils.mv params[:image].tempfile, image_path
       FileUtils.chmod 0644, image_path
       if px = params[:resize_max_pixel]
         `convert -resize #{px}x#{px} #{image_path} #{image_path}` # ImageMagickで画像をリサイズ
       end
-      render json: {file_name: file_name} and return
+      render json: {file_name: dest_file_name} and return
     else
       render json: {errors: ['画像のアップロードに失敗しました']}, status: :bad_request and return
     end
